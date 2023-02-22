51673dbccc0f6f560837db194c8c8cd3c4a5214a
shinjith-dev <shinjithpallikkara@gmail.com>
2023-02-22 19:58:45 +0530

    react, mui & basic configs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Button, Typography } from '@mui/material'
+
+function App() {
+  return (
+    <div className="App">
+      <Typography>Welcome to KEL</Typography>
+      <Button variant="outlined" color="primary">
+        {'Guide me throw the app >'}
+      </Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/theme/theme.js
@@ -0,0 +1,55 @@
+import { createTheme } from '@mui/material/styles'
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#112D4E',
+    },
+    secondary: {
+      main: '#3F72AF',
+    },
+    background: {
+      main: '#FFFFFF',
+      bg1: '#F9F7F7',
+      bg2: '#F9F7F7',
+    },
+    // as1: {
+    //   main: "#FFCCCC",
+    // },
+    // as2: {
+    //   main: "#FFE5D2",
+    // },
+    // as3: {
+    //   main: "#FFF2CC",
+    // },
+    // as4: {
+    //   main: "#E3EDD2",
+    // },
+    // as5: {
+    //   main: "#DCE2D0",
+    // },
+    // AS1: {
+    //   main: "#FF0000",
+    // },
+    // AS2: {
+    //   main: "#FF7D1E",
+    // },
+    // AS3: {
+    //   main: "#FFC000",
+    // },
+    // AS4: {
+    //   main: "#75A620",
+    // },
+    // AS5: {
+    //   main: "#4E6E15",
+    // },
+  },
+
+  text: {
+    primary: '#222222',
+    secondary: '#676767',
+    disabled: '#A6A6A6',
+  },
+})
+
+export default theme
