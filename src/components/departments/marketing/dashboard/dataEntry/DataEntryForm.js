4a2269b8ee13893c173c983928b8e9b2f7e8e96d
the3plet <soorajp2002@gmail.com>
2023-02-27 16:08:50 +0530

    DataEntryForm Text Field Added @the3plet

--- src/components/departments/marketing/dashboard/dataEntry/DataEntryForm.js
+++ src/components/departments/marketing/dashboard/dataEntry/DataEntryForm.js
@@ -12,7 +12,7 @@ import { useFormik } from 'formik'
 import React from 'react'
 
 const initialValues = {
-  // WO/SO Number
+  // WO/SO No
   woso_no: '',
   woso_date: '',
   customer: '',
@@ -26,7 +26,7 @@ const initialValues = {
   consignee_addr_line2: '',
   consignee_addr_line3: '',
   consignee_pin: '',
-  consignee_mob_no: '',
+  consignee_phone_no: '',
   consignee_gst_no: '',
 
 // Buyer
@@ -35,7 +35,7 @@ const initialValues = {
   buyer_addr_line2: '',
   buyer_addr_line3: '',
   buyer_pin: '',
-  buyer_mob_no: '',
+  buyer_phone_no: '',
   buyer_gst_no: '',
 }
 
@@ -64,7 +64,7 @@ const DataEntryForm = ({ close, handleSubmit, submitting }) => {
                   size="small"
                   id="woso_no"
                   name="woso_no"
-                  label="WO/SO Number"
+                  label="WO/SO No"
                   value={formik.values.woso_no}
                   onChange={formik.handleChange}
                   error={
@@ -123,7 +123,7 @@ const DataEntryForm = ({ close, handleSubmit, submitting }) => {
                   size="small"
                   id="po_no"
                   name="po_no"
-                  label="PO Number"
+                  label="PO No"
                   value={formik.values.po_no}
                   onChange={formik.handleChange}
                   error={formik.touched.po_no && Boolean(formik.errors.po_no)}
@@ -169,7 +169,152 @@ const DataEntryForm = ({ close, handleSubmit, submitting }) => {
             </Grid>
           </Grid>
 
+          
+
+          
+
           {/* Consignee */}
+
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="body1" fontWeight={500} sx={{ mb: 2 }}>
+              Address of Consignee
+              </Typography>
+              <FormControl sx={{ width: '100%' }}>
+                <TextField
+                  fullWidth
+                  //   disabled={submitting}
+                  size="small"
+                  id="consignee_org"
+                  name="consignee_org"
+                  label="Organization"
+                  value={formik.values.consignee_org}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.consignee_org && Boolean(formik.errors.consignee_org)
+                  }
+                  helperText={formik.touched.consignee_org && formik.errors.consignee_org}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl sx={{ width: '100%' }}>
+              <TextField
+                  fullWidth
+                  //   disabled={submitting}
+                  size="small"
+                  id="consignee_addr_line1"
+                  name="consignee_addr_line1"
+                  label="Address Line-1"
+                  value={formik.values.consignee_addr_line1}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.consignee_addr_line1 && Boolean(formik.errors.consignee_addr_line1)
+                  }
+                  helperText={formik.touched.consignee_addr_line1 && formik.errors.consignee_addr_line1}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl sx={{ width: '100%' }}>
+              <TextField
+                  fullWidth
+                  //   disabled={submitting}
+                  size="small"
+                  id="consignee_addr_line2"
+                  name="consignee_addr_line2"
+                  label="Address Line-2"
+                  value={formik.values.consignee_addr_line2}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.consignee_addr_line2 && Boolean(formik.errors.consignee_addr_line2)
+                  }
+                  helperText={formik.touched.consignee_addr_line2 && formik.errors.consignee_addr_line2}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl sx={{ width: '100%' }}>
+              <TextField
+                  fullWidth
+                  //   disabled={submitting}
+                  size="small"
+                  id="consignee_addr_line3"
+                  name="consignee_addr_line3"
+                  label="Address Line-3"
+                  value={formik.values.consignee_addr_line3}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.consignee_addr_line3 && Boolean(formik.errors.consignee_addr_line3)
+                  }
+                  helperText={formik.touched.consignee_addr_line3 && formik.errors.consignee_addr_line3}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl sx={{ width: '100%' }}>
+                <TextField
+                  fullWidth
+                  //   disabled={submitting}
+                  size="small"
+                  id="consignee_pine"
+                  name="consignee_pin"
+                  type={"number"}
+                  label="PIN"
+                  value={formik.values.consignee_pin}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.consignee_pin && Boolean(formik.errors.consignee_pin)
+                  }
+                  helperText={formik.touched.consignee_pin && formik.errors.consignee_pin}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl sx={{ width: '100%' }}>
+                <TextField
+                  fullWidth
+                  //   disabled={submitting}
+                  size="small"
+                  id="consignee_phone_no"
+                  name="consignee_phone_no"
+                  label="Phone No"
+                  value={formik.values.consignee_phone_no}
+                  onChange={formik.handleChange}
+                  error={formik.touched.consignee_phone_no && Boolean(formik.errors.consignee_phone_no)}
+                  helperText={formik.touched.consignee_phone_no && formik.errors.consignee_phone_no}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl sx={{ width: '100%' }}>
+                <TextField
+                  fullWidth
+                  //   disabled={submitting}
+                  size="small"
+                  id="consignee_gst_no"
+                  name="consignee_gst_no"
+                  label="GST No"
+                  value={formik.values.consignee_gst_no}
+                  onChange={formik.handleChange}
+                  error={formik.touched.consignee_gst_no && Boolean(formik.errors.consignee_gst_no)}
+                  helperText={formik.touched.consignee_gst_no && formik.errors.consignee_gst_no}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+          
+
+
+                  
+
+
          
         </Box>
 
