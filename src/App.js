45d407b8b9f76afbffcd141cfd3376ad4a588af8
shinjith-dev <shinjithpallikkara@gmail.com>
2023-03-20 20:52:21 +0530

    safe commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import ErrorPage from './pages/ErrorPage'
 import Home from './pages/Home'
 import Marketing from './pages/Marketing'
+import Planning from './pages/Planning'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/marketing/*" element={<Marketing />} />
+      <Route path="/planning/*" element={<Planning />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
+import { Container, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+      <Typography>KEL ERP</Typography>
+      <Link to={'/marketing'}>Marketing Department</Link><br />
+      <Link to={'/planning'}>Planning Department</Link>
+    </Container>
   )
 }
 
