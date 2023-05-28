3c11e2071ed902121a1d99ff4756dfa4a2106491
the3plet <soorajp2002@gmail.com>
2023-05-28 15:17:57 +0530

    Edit fixes

--- src/components/planning/materials/Edit.jsx
+++ src/components/planning/materials/Edit.jsx
@@ -4,6 +4,7 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  Skeleton,
   TextField,
 } from '@mui/material';
 import axios from 'axios';
@@ -15,13 +16,18 @@ function Edit() {
   const [input, setInput] = useState('');
   const [matcode, setMatcode] = useState('');
   const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (event) => {
     setInput(event.target.value);
   };
 
   const handleMatcodeChange = () => {
+    setLoading(true);
     setMatcode(input);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
   };
 
   useEffect(() => {
@@ -92,7 +98,15 @@ function Edit() {
       >
         Edit
       </Button>
-      <Dialog open={open} onClose={() => setOpen(false)}>
+      <Dialog
+        open={open}
+        onClose={() => {
+          setOpen(false);
+          formik.resetForm();
+          setMatcode('');
+          setLoading(false);
+        }}
+      >
         <DialogTitle>Material Editor</DialogTitle>
         <DialogContent>
           <TextField
@@ -115,165 +129,192 @@ function Edit() {
               Search
             </Button>
           </DialogActions>
-
-          <form
-            style={{
-              border: '2px solid #48466d',
-              padding: '15px',
-              borderRadius: '10px',
-            }}
-          >
-            <TextField
-              fullWidth
-              margin="dense"
-              id="matcode"
-              name="matcode"
-              label="Matcode"
-              variant="outlined"
-              size="small"
-              value={formik.values.matcode}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="title"
-              name="title"
-              label="Title"
-              variant="outlined"
-              size="small"
-              value={formik.values.title}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="ref"
-              name="ref"
-              label="Ref"
-              variant="outlined"
-              size="small"
-              value={formik.values.ref}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="au"
-              name="au"
-              label="au"
-              variant="outlined"
-              size="small"
-              value={formik.values.au}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="safstk"
-              name="safstk"
-              label="safstk"
-              variant="outlined"
-              size="small"
-              value={formik.values.safstk}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="ordercst"
-              name="ordercst"
-              label="ordercst"
-              variant="outlined"
-              size="small"
-              value={formik.values.ordercst}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="safty"
-              name="safty"
-              label="safty"
-              variant="outlined"
-              size="small"
-              value={formik.values.safty}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="spare"
-              name="spare"
-              label="spare"
-              variant="outlined"
-              size="small"
-              value={formik.values.spare}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="matgroup"
-              name="matgroup"
-              label="matgroup"
-              variant="outlined"
-              size="small"
-              value={formik.values.matgroup}
-              onChange={formik.handleChange}
-            />
-            <TextField
-              fullWidth
-              margin="dense"
-              id="abc"
-              name="abc"
-              label="abc"
-              variant="outlined"
-              size="small"
-              value={formik.values.abc}
-              onChange={formik.handleChange}
-            />
-
-            <div style={{ display: 'flex', gap: '10px' }}>
-              <TextField
-                fullWidth
-                margin="dense"
-                id="reorderqty"
-                name="reorderqty"
-                label="reorderqty"
-                variant="outlined"
-                size="small"
-                value={formik.values.reorderqty}
-                onChange={formik.handleChange}
-              />
-
-              <TextField
-                fullWidth
-                margin="dense"
-                id="unitrate"
-                name="unitrate"
-                label="unitrate"
-                variant="outlined"
-                size="small"
-                value={formik.values.unitrate}
-                onChange={formik.handleChange}
-              />
-              <TextField
-                fullWidth
-                margin="dense"
-                id="dwgno"
-                name="dwgno"
-                label="dwgno"
-                variant="outlined"
-                size="small"
-                value={formik.values.dwgno}
-                onChange={formik.handleChange}
-              />
+          {loading ? (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '5px',
+              }}
+            >
+              <Skeleton variant="rounded" width="518px" height="50px" />
+              <Skeleton animation="wave" />
+              <Skeleton variant="rounded" width="518px" height="50px" />
+              <Skeleton animation="wave" />
+              <Skeleton variant="rounded" width="518px" height="50px" />
+              <Skeleton animation="wave" />
             </div>
-          </form>
+          ) : (
+            matcode.length >= 6 && (
+              <form
+                style={{
+                  border: '2px solid #48466d',
+                  padding: '15px',
+                  borderRadius: '10px',
+                }}
+              >
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="matcode"
+                  name="matcode"
+                  label="Matcode"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.matcode}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="title"
+                  name="title"
+                  label="Title"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.title}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="ref"
+                  name="ref"
+                  label="Ref"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.ref}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="au"
+                  name="au"
+                  label="au"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.au}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="safstk"
+                  name="safstk"
+                  label="safstk"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.safstk}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="ordercst"
+                  name="ordercst"
+                  label="ordercst"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.ordercst}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="safty"
+                  name="safty"
+                  label="safty"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.safty}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="spare"
+                  name="spare"
+                  label="spare"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.spare}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="matgroup"
+                  name="matgroup"
+                  label="matgroup"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.matgroup}
+                  onChange={formik.handleChange}
+                />
+                <TextField
+                  fullWidth
+                  margin="dense"
+                  id="abc"
+                  name="abc"
+                  label="abc"
+                  variant="outlined"
+                  size="small"
+                  value={formik.values.abc}
+                  onChange={formik.handleChange}
+                />
+
+                <div style={{ display: 'flex', gap: '10px' }}>
+                  <TextField
+                    fullWidth
+                    margin="dense"
+                    id="reorderqty"
+                    name="reorderqty"
+                    label="reorderqty"
+                    variant="outlined"
+                    size="small"
+                    value={formik.values.reorderqty}
+                    onChange={formik.handleChange}
+                  />
+
+                  <TextField
+                    fullWidth
+                    margin="dense"
+                    id="unitrate"
+                    name="unitrate"
+                    label="unitrate"
+                    variant="outlined"
+                    size="small"
+                    value={formik.values.unitrate}
+                    onChange={formik.handleChange}
+                  />
+                  <TextField
+                    fullWidth
+                    margin="dense"
+                    id="dwgno"
+                    name="dwgno"
+                    label="dwgno"
+                    variant="outlined"
+                    size="small"
+                    value={formik.values.dwgno}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+              </form>
+            )
+          )}
         </DialogContent>
         <DialogActions>
-          <Button variant="contained" onClick={() => setOpen(false)}>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setOpen(false);
+              setOpen(false);
+              formik.resetForm();
+              setMatcode('');
+              setLoading(false);
+            }}
+          >
             Close
           </Button>
           <Button
